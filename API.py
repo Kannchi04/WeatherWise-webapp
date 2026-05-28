c5ac475a0804d0dc5864a3426d8dc216a794717e
Kannchi04 <94986556+Kannchi04@users.noreply.github.com>
2026-05-28 22:55:14 +0530

    Update API.py

--- API.py
+++ API.py
@@ -1,13 +1,28 @@
 import requests
 
-API_KEY = "8f1f0fc87531ec5f381cb58eca0c83f8"
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+API_KEY = "ENTER_YOUR_API_KEY"
+BASE_URL = "ENTER_YOUR_BASE_URL"
+
 
 def get_weather(city):
-    params = {"q": city, "appid": API_KEY, "units": "metric"}
-    response = requests.get(BASE_URL, params=params)
-    return response.json()
+    params = {
+        "q": city,
+        "appid": API_KEY,
+        "units": "metric"
+    }
 
-city = "Indore"
-data = get_weather(city)
-print(data)
+    response = requests.get(BASE_URL, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {
+            "cod": response.status_code,
+            "message": response.json().get("message", "Error fetching weather data")
+        }
+
+#test
+if __name__ == "__main__":
+    city = "Indore"
+    data = get_weather(city)
+    print(data)
